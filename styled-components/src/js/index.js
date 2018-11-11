5a54606cdcdbca6edde498afa54de1be8184adc1
ssmith-wombatweb <ssmith@wombatweb.us>
2018-11-11 14:18:36 -0800

    Completed sass setup.

--- styled-components/src/js/index.js
+++ styled-components/src/js/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styled from 'styled-components';
 
-const RedDiv = styled.div`
-  background-color: red;
-`
+import Ghost from './Ghost.js'
 
 const Index = () => {
-  return <RedDiv>Hello React!</RedDiv>;
+  return (
+    <>
+      <Ghost ghost="blinky" quadrant="top-left" timing="move-first" />
+      <Ghost ghost="pinky" quadrant="top-right" timing="move-fourth" />
+      <Ghost ghost="clyde" quadrant="bottom-left" timing="move-second" />
+      <Ghost ghost="inky" quadrant="bottom-right" timing="move-third" />
+    </>
+  );
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
