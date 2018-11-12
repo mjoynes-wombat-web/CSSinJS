c0ede280988cc9913f07e318ec538e836323123f
ssmith-wombatweb <ssmith@wombatweb.us>
2018-11-11 16:17:41 -0800

    Setup styled-jsx example.

--- styled-jsx/src/js/Ghost.js
+++ styled-jsx/src/js/Ghost.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { ghostMain, ghostFeet, ghostMouth, ghostBody, ghostEye, ghostAnimations } from './Ghost.css';
+
+const ghostColors = {
+  blinky: "red",
+  pinky: "pink",
+  clyde: "orange",
+  inky: "cyan",
+}
+
 class Ghost extends Component {
   state = {
     clicked: false,
@@ -20,24 +29,39 @@ class Ghost extends Component {
     const { ghost, quadrant, timing } = this.props;
     const { clicked, reviving } = this.state;
     return (
-      <div role="button" onClick={this.clicked} className={`ghost ${ghost} ${quadrant} ${timing} ${clicked ? "clicked" : ""} ${reviving ? "reviving" : ""}`}>
-        <div className="ghost__body">
+      <div role="button" onClick={this.clicked} className={`${ghostMain.className} ghost ${quadrant} ${timing} ${clicked ? "clicked" : ""} ${reviving ? "reviving" : ""}`}>
+        <style jsx>{`
+          .ghost {
+            color: ${ghostColors[ghost]};
+
+            .${ghostFeet.className} {
+              background: ${ghostColors[ghost]};
+            }
+          }
+        `}</style>
+        <style jsx global>{ghostAnimations}</style>
+        {ghostMain.styles}
+        {ghostFeet.styles}
+        {ghostMouth.styles}
+        {ghostBody.styles}
+        {ghostEye.styles}
+        <div className={ghostBody.className}>
           <svg width="56px" height="56px">
             <polygon points="0 24, 4 24, 4 12, 8 12, 8 8, 12 8, 12 4, 20 4, 20 0, 36 0, 36 4, 44 4, 44 8, 48 8, 48 12, 52 12, 52 24, 56 24, 56 48, 0 48 " />
           </svg>
         </div>
-        <div className="ghost__eye--left">
+        <div className={`${ghostEye.className} left`}>
           <svg>
             <polygon points="4 0, 12 0, 12 4, 16 4, 16 16, 12 16, 12 20, 4 20, 4 16, 0 16, 0 4, 4 4 " />
           </svg>
         </div>
-        <div className="ghost__eye--right">
+        <div className={`${ghostEye.className} right`}>
           <svg>
             <polygon points="4 0, 12 0, 12 4, 16 4, 16 16, 12 16, 12 20, 4 20, 4 16, 0 16, 0 4, 4 4 " />
           </svg>
         </div>
-        <div className="ghost__mouth"></div>
-        <div className="ghost__feet"></div>
+        <div className={ghostMouth.className}></div>
+        <div className={ghostFeet.className}></div>
       </div>
     );
   }
--- styled-jsx/src/js/index.js
+++ styled-jsx/src/js/index.js
@@ -6,6 +6,13 @@ import Ghost from './Ghost.js'
 const Index = () => {
   return (
     <>
+      <style jsx global>
+        {`
+          body {
+            background: #000;
+          }
+        `}
+      </style>
       <Ghost ghost="blinky" quadrant="top-left" timing="move-first" />
       <Ghost ghost="pinky" quadrant="top-right" timing="move-fourth" />
       <Ghost ghost="clyde" quadrant="bottom-left" timing="move-second" />
